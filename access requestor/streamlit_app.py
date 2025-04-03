c22eda7cae285fe271f5e887b6e67353428d3a6a
Andy Blaney <andy.blaney@snowflake.com>
2025-04-04 12:59:15 +1300

    remove show grants

--- access requestor/streamlit_app.py
+++ access requestor/streamlit_app.py
@@ -128,7 +128,7 @@ if submit_request:
         insert_request_mins(session, sf_database, sf_schema, user, role_requested, mins_requested, request_reason)
         st.success('Request Submitted')
     
-    # email_requestor()
+    email_requestor()
 
 st.subheader('Requests from user - last 30 days')
 df_open_requests = get_open_requests_for_user(session, sf_database, sf_schema, user)
